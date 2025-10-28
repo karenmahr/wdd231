03721d3ed7519bc1824d4dae2d8950daf0a64fb1
karenmahr <kmahr@byupathway.edu>
2025-10-27 21:17:01 -0300

    Added the site navigation and footer icons

--- week01/scripts/site-header.js
+++ week01/scripts/site-header.js
@@ -2,4 +2,7 @@ const navButton = document.querySelector('#nav-button');
 
 navButton.addEventListener('click', () => {
     navButton.classList.toggle('show');
+    navBar.classList.toggle('show');
 });
+
+const navBar = document.querySelector('#nav-bar');
